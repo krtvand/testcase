31ac9c1ade807dbdd1cb520b247a01a7f75873f2
krtvand <krtvand@gmail.com>
2017-02-10 14:18:26 +0300

    parcel model

--- first/admin.py
+++ first/admin.py
@@ -15,8 +15,15 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('article', 'vendor', 'name',
                     'barcode', 'image', 'price', 'weight')
 
+
+class RecipientAdmin(admin.ModelAdmin):
+    model = models.Recipient
+    list_display = ('fullname', 'address', 'photo',
+                    'registration_date')
+
 admin.site.register(models.BarcodeType)
 admin.site.register(models.Barcode, BarcodeAdmin)
 admin.site.register(models.Vendor)
 # TODO сделать поле Barcode типа Inline
 admin.site.register(models.Product, ProductAdmin)
+admin.site.register(models.Recipient, RecipientAdmin)
--- first/models.py
+++ first/models.py
@@ -59,10 +59,11 @@ class Product(models.Model):
     article = models.CharField(max_length=20)
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    barcode = models.ForeignKey(Barcode, on_delete=models.CASCADE, blank=True, null=True)
-    image = models.ImageField(upload_to='product_images/', blank=True, null=True)
-    # price = models.ForeignKey(Price)
-    # weight = models.ForeignKey(Weight)
+    barcode = models.ForeignKey(Barcode,
+                                on_delete=models.CASCADE,
+                                blank=True, null=True)
+    image = models.ImageField(upload_to='product_images/',
+                              blank=True, null=True)
     price = models.DecimalField(max_digits=9, decimal_places=2)
     weight = models.DecimalField(max_digits=9, decimal_places=2)
 
@@ -73,7 +74,13 @@ class Product(models.Model):
 class Fullname(models.Model):
     first_name = models.CharField(max_length=30)
     surname = models.CharField(max_length=30)
-    patronymic = models.CharField(max_length=30, blank=True, null=True)
+    patronymic = models.CharField(max_length=30,
+                                  blank=True, null=True)
+
+    def __str__(self):
+        return '{} {} {}'.format(self.surname,
+                                 self.first_name, self.patronymic)
+
 
 class Address(models.Model):
     # TODO продумать тип адреса, чтобы был интернациональный
@@ -83,10 +90,34 @@ class Address(models.Model):
     city = models.CharField(max_length=30, blank=True)
     address = models.CharField(max_length=100)
 
+    def __str__(self):
+        return '{}, {}, {}, {}'.format(self.postal_code,
+                                       self.country, self.city,
+                                       self.address)
+
 
 class Recipient(models.Model):
     fullname = models.ForeignKey(Fullname, on_delete=models.CASCADE)
     address = models.ForeignKey(Address)
-    photo = models.ImageField(upload_to='recipient_photos/')
+    photo = models.ImageField(upload_to='recipient_photos/',
+                              blank=True, null=True)
     registration_date = models.DateField(auto_now=True)
 
+    def __str__(self):
+        return '{}, {}'.format(self.fullname, self.address)
+
+
+class Parcel(models.Model):
+    products = models.ManyToManyField(Product)
+    recipient = models.ForeignKey(Recipient, on_delete=models.CASCADE)
+    isdelivered = models.BooleanField()
+    isrefused = models.BooleanField()
+    departure_date = models.DateTimeField()
+    delivery_date = models.DateTimeField()
+    cost_of_delivery = models.DecimalField(max_digits=9,
+                                           decimal_places=2)
+
+    def __str__(self):
+        return '{}, {}, {}'.format(self.recipient,
+                                   self.departure_date,
+                                   self.cost_of_delivery)
